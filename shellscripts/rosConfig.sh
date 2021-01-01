391392ca3cbc9534adf6090f66c3a0bc1af93b00
DaQuiTree <dhsxmy1314@163.com>
2021-01-01 16:04:44 +0800

    baidu_speech依赖

---
+++ shellscripts/rosConfig.sh
@@ -0,0 +1,7 @@
+export LANG=en_US.utf8
+source /opt/ros/kinetic/setup.bash
+ROS_IP=127.0.0.1
+ROS_MASTER_URI=http://127.0.0.1:11311
+export ROS_PACKAGE_PATH=/opt/ros/kinetic/share
+export PYTHON_VLC_LIB_PATH=/usr/lib/libvlc.so
+
