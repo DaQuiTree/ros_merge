d595a001c96f092432237e45f26e699845dc7c39
DaQuiTree <dhsxmy1314@163.com>
2021-02-04 18:06:44 +0800

    修改logs路径

--- config/buildroot/target/root/etcsXG/common/cron_start.sh
+++ config/buildroot/target/root/etcsXG/common/cron_start.sh
@@ -1,9 +1,9 @@
 mkdir -p /var/spool/cron/crontabs
 echo "root" >> /var/spool/cron/crontabs/cron.update
 #check wifi state one minute, and reconnect
-echo "* * * * * /root/etcs/WIFI_Check.sh" >> /var/spool/cron/crontabs/root
+echo "* * * * * /root/etcsXG/WIFI_Check.sh" >> /var/spool/cron/crontabs/root
 #clean logs a day
 echo "* * */1 * * rm -rf /root/.ros/log/*" >> /var/spool/cron/crontabs/root
 echo "* * */1 * * rm -rf /.ros/log/*" >> /var/spool/cron/crontabs/root
 /usr/sbin/crond &
-#echo "* * * * * /root/etcs/WIFI_Check.sh >/dev/ttyFIQ0 2>&1" > /var/spool/cron/crontabs/root
+#echo "* * * * * /root/etcsXG/WIFI_Check.sh >/dev/ttyFIQ0 2>&1" > /var/spool/cron/crontabs/root
---
+++ config/buildroot/target/root/etcsXG/machine47_85/app_start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export LANG=en_US.utf8
+source /opt/ros/kinetic/setup.bash
+ROS_IP=192.168.1.47
+ROS_MASTER_URI=http://192.168.1.47:11311
+export ROS_PACKAGE_PATH=/opt/ros/kinetic/share
+export PYTHON_VLC_LIB_PATH=/usr/lib/libvlc.so
+
+#roslaunch robot_machine other.launch >/root/logsXG/other.txt 2>&1 &
+#sleep 15
+
+roslaunch kobuki_node minimal.launch >/root/logsXG/kobuki.log 2>&1 &
+sleep 10
+#sleep 15
+
+#roslaunch xlc_to_laserscan lidar.launch >/root/logsXG/xlc.log 2>&1 &
+#sleep 2
+
+roslaunch ydlidar lidar.launch >/root/logsXG/ydlidar.log 2>&1 &
+#sleep 2
+
+roslaunch baidu_speech simple_speaker.launch >/root/logsXG/baidu.log 2>&1 &
+#sleep 2
+
+rosrun message_forward message_forward_node > /root/logsXG/message_forward.log 2>&1 &
+#sleep 2
+
+rosrun robot_machine robot_machine_node > /root/logsXG/robot_machine.log 2>&1 &
+#sleep 2
+
+roslaunch topband_app topband_app.launch >/root/logsXG/topband_app.log 2>&1 &
+#sleep 2
+
+rosrun docking docking_node >/root/logsXG/docking.log 2>&1 &
+#sleep 2
+
+roslaunch global_clean_task global_clean_task.launch >/root/logsXG/global_clean_task.log 2>&1 &
+#sleep 5
+
+roslaunch nav_p2p_planner nav_p2p_planner.launch >/root/logsXG/nav_p2p.log 2>&1 &
+#sleep 5
+
+roslaunch gmapping slam_gmapping_pr2.launch > /root/logsXG/gmapping.log 2>&1 &
+#sleep 5
+
